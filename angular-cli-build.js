313f11bc0a45e2298de995132da7170584ba0f89
vik-13 <vik.ugrin@gmail.com>
2016-06-09 18:35:58 +0300

    updates

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -11,7 +11,10 @@ module.exports = function(defaults) {
       'es6-shim/es6-shim.js',
       'reflect-metadata/**/*.+(js|js.map)',
       'rxjs/**/*.+(js|js.map)',
-      '@angular/**/*.+(js|js.map)'
+      '@angular/**/*.+(js|js.map)',
+      '@angular2-material/**/*.+(css|js|js.map)',
+      'firebase/lib/firebase-web.js',
+      'angularfire2/angularfire2.+(js|js.map)'
     ]
   });
 };
